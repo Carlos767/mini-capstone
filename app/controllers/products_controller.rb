a55a5b37ed579349bd7afe5b9d7744b67ca60b50
Carlos <school@Carlos-MacBook-Air.local>
2017-01-23 18:11:33 -0800

    Lots of changes!

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,51 @@
 class ProductsController < ApplicationController
 
-  def get_products
-    @first_product = Product.first
+  def index
     @products = Product.all
-    render "products.html.erb"
+    render "index.html.erb"
   end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    product = Product.create(
+      name: params[:name], 
+      price: params[:price],
+      image: params[:image],
+      description: params[:description])
+    render "create.html.erb"
+  end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "show.html.erb"
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "edit.html.erb"
+  end
+
+  def update
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.name = params[:name]
+    product.save
+    render "update.html.erb"
+  end
+
+  def destroy
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.destroy
+    render "destroy.html.erb"
+  end
+
+
+    
 
 end
