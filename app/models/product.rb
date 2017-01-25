d7571ba0b943014e926f5b03e587eb9bb90769f2
Carlos <school@Carlos-MacBook-Air.local>
2017-01-24 19:28:41 -0800

    layout and other adjustments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+  def description_list
+    return descriptions.split(",")
+  end
+
+  def friendly_created_at
+    created_at.striftime("%A %B %e %Y")
+  end
+
 end
